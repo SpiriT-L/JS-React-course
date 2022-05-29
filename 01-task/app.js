90db8147d7a4ee8751e20c4fe85c257fa221820d
LeonID <lmatskevich79@gmail.com>
2022-05-30 00:15:01 +0300

    init: init 01 task

---
+++ 01-task/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let money = 'Ваш бюджет на месяц';
+let time = 'Введите дату в формате YYYY-MM-DD';
+
+let appData = {
+  budget: money,
+  timeData: time,
+  expenses: {
+    ''
+  }
+}
